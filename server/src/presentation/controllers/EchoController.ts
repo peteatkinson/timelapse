73682b6e585c40eac4dc744b7f6744d89fb64630
Peter <peteratkinson1994@gmail.com>
2021-10-03 22:36:24 +0100

    feat: Add organisation and department prisma models

--- server/src/presentation/controllers/EchoController.ts
+++ server/src/presentation/controllers/EchoController.ts
@@ -1,10 +1,20 @@
 import { HttpResponse, Controller } from '@/presentation/protocols'
 
+import { PrismaClient } from '@prisma/client';
+
 export class EchoController implements Controller {
   async handle (text: string): Promise<HttpResponse> {
+
+    const { user } = new PrismaClient();
+    const users = await user.findMany({
+      select: {
+        username: true
+      }
+    })
+
     return {
       statusCode: 200,
-      body: text
+      body: users
     }
   }
 }
