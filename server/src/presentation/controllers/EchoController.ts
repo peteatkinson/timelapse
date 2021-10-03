c3d318602f48b72829cccc0f966a0b07cd533929
Peter <peteratkinson1994@gmail.com>
2021-10-03 23:36:45 +0100

    fix: formatting of file

--- server/src/presentation/controllers/EchoController.ts
+++ server/src/presentation/controllers/EchoController.ts
@@ -4,9 +4,19 @@ import { PrismaClient } from '@prisma/client';
 
 export class EchoController implements Controller {
   async handle (text: string): Promise<HttpResponse> {
+
+    const { department } = new PrismaClient()
+    const departments = await department.findMany({
+      where: {
+        organisationId: {
+          equals: '00d9f061-e243-4721-b174-7e0bd577cf14'
+        }
+      }
+    })
+
     return {
       statusCode: 200,
-      body: text
+      body: departments
     }
   }
 }
