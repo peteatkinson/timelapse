ea13517f20dbb963a8f797a598f2edaf40e9c955
Peter <peteratkinson1994@gmail.com>
2021-10-06 22:07:51 +0100

    Add entity mappers to organisation repository

--- server/src/domain/repositories/organisation.ts
+++ server/src/domain/repositories/organisation.ts
@@ -1,18 +1,29 @@
-import prisma from '@/lib/prisma';
-import { Organisation } from '.prisma/client';
+import { OrganisationEntity } from '@/domain/entities';
+import { PrismaClient } from '@prisma/client';
 
 export class OrganisationRepository {
-  async loadOrganisation(id: string): Promise<Organisation> {
-    return await prisma.organisation.findFirst({
-      where: {
-        id: {
-          equals: id,
+  async loadOrganisation(id: string): Promise<OrganisationEntity> {
+    const { organisation } = new PrismaClient();
+    return OrganisationEntity.map(
+      await organisation.findFirst({
+        where: {
+          id: {
+            equals: id,
+          },
         },
-      },
-    });
+      }),
+    );
   }
 
-  async createOrganisation(organisation: Organisation): Promise<Organisation> {
-    return await prisma.organisation.create({data: organisation});
+  async createOrganisation(data: OrganisationEntity): Promise<string> {
+    const { organisation } = new PrismaClient();
+    const entity = OrganisationEntity.map(
+      await organisation.create({
+        data: {
+          name: data.name
+        },
+      }),
+    );
+    return entity?.id;
   }
 }
