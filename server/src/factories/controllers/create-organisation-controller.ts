71868956bdcf57e0a04c1702edcec345ff780ba6
Peter <peteratkinson1994@gmail.com>
2021-10-06 22:23:57 +0100

    Add command handler to CreateOrganisationController

--- server/src/factories/controllers/create-organisation-controller.ts
+++ server/src/factories/controllers/create-organisation-controller.ts
@@ -1,6 +1,10 @@
 import { Controller } from '@/presentation/protocols'
 import { CreateOrganisationController } from '@/presentation/controllers'
+import { CreateOrganisationCommandHandler } from '@/commands/create-organisation';
+import { OrganisationRepository } from '@/domain/repositories/organisation';
 
 export const makeCreateOrganisationController = (): Controller => {
-  return new CreateOrganisationController();
+  const organisationRepository = new OrganisationRepository();
+  const organisationCommandHandler = new CreateOrganisationCommandHandler(organisationRepository);
+  return new CreateOrganisationController(organisationCommandHandler);
 }
