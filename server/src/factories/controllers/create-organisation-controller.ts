f3b59a4f7743ad18542c48d555c378353b014e6c
Peter <peteratkinson1994@gmail.com>
2021-10-10 05:09:30 +0100

    Remove server folder

--- server/src/factories/controllers/create-organisation-controller.ts
+++
@@ -1,6 +0,0 @@
-import { Controller } from '@/presentation/protocols'
-import { CreateOrganisationController } from '@/presentation/controllers'
-
-export const makeCreateOrganisationController = (): Controller => {
-  return new CreateOrganisationController();
-}
